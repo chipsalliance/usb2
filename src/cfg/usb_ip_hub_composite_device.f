80d67da16f33d7d3afea8a60ec58139d12ecc03d
Bart Vertenten <33702777+mister-gueuze@users.noreply.github.com>
2026-09-14 13:34:28 +0200

    Update usb_ip_hub_composite_device.f
    Updated RTL file list to match files used in PR#21 for USB hub compound device

--- src/cfg/usb_ip_hub_composite_device.f
+++ src/cfg/usb_ip_hub_composite_device.f
@@ -3,19 +3,16 @@ ${USB_ROOT}/usb_hub_composite_device/RTL/INTERFACE/usb_general_subcmp_pkg.p.vhdl
 ${USB_ROOT}/usb_hub_composite_device/RTL/INTERFACE/usb_subcmp_pkg.p.vhdl
 ${USB_ROOT}/usb_hub_composite_device/RTL/INTERFACE/usb_configuration_subcmp_pkg.p.vhdl
 ${USB_ROOT}/usb_hub_composite_device/RTL/INTERFACE/usb_ep_config_hub_pkg.p.vhdl
-${USB_ROOT}/usb_hub_composite_device/RTL/INTERFACE/usb_fs_emb_dev_pkg.p.vhdl
 
-${USB_ROOT}/usb_hub_composite_device/RTL/RTL/usb_clock_gate_rtl.m.vhdl
-${USB_ROOT}/usb_hub_composite_device/RTL/RTL/usb_clock_divider.m.vhdl
 ${USB_ROOT}/usb_hub_composite_device/RTL/RTL/ahb_dma_slave.m.vhdl
 ${USB_ROOT}/usb_hub_composite_device/RTL/RTL/usb_ahb_slave.m.vhdl
 ${USB_ROOT}/usb_hub_composite_device/RTL/RTL/usb_reg_if.m.vhdl
 ${USB_ROOT}/usb_hub_composite_device/RTL/RTL/usb_dma.m.vhdl
 ${USB_ROOT}/usb_hub_composite_device/RTL/RTL/usb_pie.m.vhdl
 ${USB_ROOT}/usb_hub_composite_device/RTL/RTL/usb_synchronizer.m.vhdl
-${USB_ROOT}/usb_hub_composite_device/RTL/RTL/usb_upstreamled.m.vhdl
-${USB_ROOT}/usb_hub_composite_device/RTL/RTL/usb_fs_mux.m.vhdl
+${USB_ROOT}/usb_hub_composite_device/RTL/RTL/usb_mux.m.vhdl
 ${USB_ROOT}/usb_hub_composite_device/RTL/RTL/usb_ep0_handler.m.vhdl
+${USB_ROOT}/usb_hub_composite_device/RTL/RTL/usb_ep0_hub_descr.m.vhdl
 ${USB_ROOT}/usb_hub_composite_device/RTL/RTL/usb_ep_config_handler.m.vhdl
 ${USB_ROOT}/usb_hub_composite_device/RTL/RTL/usb_app_hw_hub.m.vhdl
 
